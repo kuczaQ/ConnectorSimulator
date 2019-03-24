b5f2a75dc273af6c7a0bbd7850f67b5acbcefc04
Adam Mazurkiewicz-Madej <kuczaQ@gmail.com>
2019-03-24 17:10:34 +0000

    test done?

--- srcTest/RestConnectorTest.java
+++ srcTest/RestConnectorTest.java
@@ -7,28 +7,27 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@SuppressWarnings("FieldCanBeLocal")
 @RunWith(OrderedRunner.class)
 public class RestConnectorTest {
-//    private static RandomStringGenerator randomStringGenerator = new RandomStringGenerator();
+    private static RandomStringGenerator randomStringGenerator = new RandomStringGenerator();
     private static RestConnector restConnector;
 
     private static       String USER_KEY       = "atester420".toUpperCase(),
                                 USER_EMAIL     = "atester420@ventum.com",
                                 USER_NAME      = "Adam",
-                                USER_LAST_NAME = "Tester",
-                                USER_PASSWORD  = "Ventum2018";
+                                USER_LAST_NAME = "Tester";
 
     
     private static       String USER_UPDATE_EMAIL     = "mwazowski999@ventum.com",
                                 USER_UPDATE_NAME      = "Marek",
-                                USER_UPDATE_LAST_NAME = "Wazowski",
-                                USER_UPDATE_PASSWORD  = "Ventum2999";
+                                USER_UPDATE_LAST_NAME = "Wazowski";
 
     static {
         restConnector = new RestConnector(
-                "IDMSFP010",
-                "",
-                "http://localhost:8090/")
+                "not used",
+                "also not used",
+                "http://localhost:8080/")
                 {
                     @Override
                     protected String getIdentityAttribute() {
@@ -47,53 +46,53 @@ public class RestConnectorTest {
     @Test
     @Order(10)
     public void getJsonUserDataAsJSONArray() throws IOException, JSONException {
-        digiReRestConnector.getJsonUserDataAsJSONArray();
+        restConnector.getJsonUserDataAsJSONArray();
     }
-ProxyClient
+
     @Test
     @Order(20)
     public void getObjectsMap() {
-        digiReRestConnector.getObjectsMap().size();
+        restConnector.getObjectsMap().size();
     }
 
     @Test
     @Order(30)
     public void createUser() throws JSONException, IOException, ClassNotFoundException {
         Map<String, Object> userCreateMap = new HashMap<>();
-        userCreateMap.put(DigiReRestConnector.getAttrKey(),      USER_KEY);
-        userCreateMap.put(DigiReRestConnector.getAttrEmail(),    USER_EMAIL);
-        userCreateMap.put(DigiReRestConnector.getAttrName(),     USER_NAME);
-        userCreateMap.put(DigiReRestConnector.getAttrLastName(), USER_LAST_NAME);
-        userCreateMap.put(digiReRestConnector.getAttrPassword(), USER_PASSWORD);
+        userCreateMap.put(getAttrKey(),      USER_KEY);
+        userCreateMap.put(getAttrEmail(),    USER_EMAIL);
+        userCreateMap.put(getAttrName(),     USER_NAME);
+        userCreateMap.put(getAttrLastName(), USER_LAST_NAME);
 
 
-        digiReRestConnector.createUser(userCreateMap);
+        restConnector.createUser(userCreateMap);
 
-        Map<String, Object> createdUser = digiReRestConnector.getObjectsMap().get(USER_KEY);
+        Map<String, Object> createdUser = restConnector.getUser(USER_KEY);
 
         if (mapDoesntContainElements(
                 userCreateMap,
                 createdUser,
-                DigiReRestConnector.getAttrKey(),
-                DigiReRestConnector.getAttrEmail(),
-                DigiReRestConnector.getAttrName(),
-                DigiReRestConnector.getAttrLastName()))
+                getAttrKey(),
+                getAttrEmail(),
+                getAttrName(),
+                getAttrLastName()))
             throw new AssertionError("user != createdUser");
     }
+
 
     @Test
     @Order(35)
     public void getUser() throws IOException, JSONException {
-        Logger logger = digiReRestConnector.getLogger();
+        Logger logger = restConnector.getLogger();
 
         String userName = USER_KEY;
 
-        Map user = digiReRestConnector.getUser(userName);
+        Map user = restConnector.getUser(userName);
 
         logger.info(user);
 
-        if (user == null)
-            throw new AssertionError("user == null");
+        if (user == null || user.isEmpty())
+            throw new AssertionError("user is null or empty");
     }
 
     @Test
@@ -101,41 +100,30 @@ ProxyClient
     public void updateUser() throws IOException, ClassNotFoundException {
         Map<String, Object> userUpdateMap = new HashMap<>();
 
-        userUpdateMap.put(DigiReRestConnector.getAttrKey(),      USER_KEY);
-        userUpdateMap.put(DigiReRestConnector.getAttrEmail(),    USER_UPDATE_EMAIL);
-        userUpdateMap.put(DigiReRestConnector.getAttrName(),     USER_UPDATE_NAME);
-        userUpdateMap.put(DigiReRestConnector.getAttrLastName(), USER_UPDATE_LAST_NAME);
+        userUpdateMap.put(getAttrKey(),      USER_KEY);
+        userUpdateMap.put(getAttrEmail(),    USER_UPDATE_EMAIL);
+        userUpdateMap.put(getAttrName(),     USER_UPDATE_NAME);
+        userUpdateMap.put(getAttrLastName(), USER_UPDATE_LAST_NAME);
         
-        digiReRestConnector.updateUser(USER_KEY, userUpdateMap);
+        restConnector.updateUser(USER_KEY, userUpdateMap);
 
-        Map<String, Object> updatedUser = digiReRestConnector.getObjectsMap().get(USER_KEY);
+        Map<String, Object> updatedUser = restConnector.getObjectsMap().get(USER_KEY);
 
         if (mapDoesntContainElements(
                 userUpdateMap,
                 updatedUser,
-                DigiReRestConnector.getAttrEmail(),
-                DigiReRestConnector.getAttrName(),
-                DigiReRestConnector.getAttrLastName()))
+                getAttrEmail(),
+                getAttrName(),
+                getAttrLastName()))
             throw new AssertionError("user == updatedUser");
-    }
-
-    @Test
-    @Order(50)
-    public void updateSetPassword() throws IOException, ClassNotFoundException {
-        Map<String, Object> updatePasswordMap = new HashMap<>();
-
-        updatePasswordMap.put(DigiReRestConnector.getAttrKey(),      USER_KEY);
-        updatePasswordMap.put(digiReRestConnector.getAttrPassword(), USER_UPDATE_PASSWORD);
-
-        digiReRestConnector.updateSetPassword(USER_KEY, updatePasswordMap);
     }
 
     @Test
     @Order(60)
     public void deleteUser() throws IOException, ClassNotFoundException {
-        digiReRestConnector.deleteUser(USER_KEY);
+        restConnector.deleteUser(USER_KEY);
 
-        Map<String, Object> deletedUser = digiReRestConnector.getObjectsMap().get(USER_KEY);
+        Map<String, Object> deletedUser = restConnector.getObjectsMap().get(USER_KEY);
 
         if (deletedUser != null)
             throw new AssertionError("Deleted user still exists!");
@@ -154,13 +142,13 @@ ProxyClient
 
         final String userKey = randomStringGenerator.getUserName();
 
-        userUpdateMap.put(DigiReRestConnector.getAttrKey(),      userKey);
-        userUpdateMap.put(DigiReRestConnector.getAttrEmail(),    randomStringGenerator.getEmail());
-        userUpdateMap.put(DigiReRestConnector.getAttrName(),     randomStringGenerator.getString());
-        userUpdateMap.put(DigiReRestConnector.getAttrLastName(), randomStringGenerator.getString());
+        userUpdateMap.put(getAttrKey(),      userKey);
+        userUpdateMap.put(getAttrEmail(),    randomStringGenerator.getEmail());
+        userUpdateMap.put(getAttrName(),     randomStringGenerator.getString());
+        userUpdateMap.put(getAttrLastName(), randomStringGenerator.getString());
 
         try {
-            digiReRestConnector.updateUser(userKey, userUpdateMap);
+            restConnector.updateUser(userKey, userUpdateMap);
         } catch (Exception e) {
             return;
         }
@@ -188,5 +176,21 @@ ProxyClient
                 return true;
 
         return false;
+    }
+
+    private String getAttrLastName() {
+        return "lastName";
+    }
+
+    private String getAttrName() {
+        return "firstName";
+    }
+
+    private String getAttrEmail() {
+        return "email";
+    }
+
+    private String getAttrKey() {
+        return "id";
     }
 }
